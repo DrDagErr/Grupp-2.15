1e73d6f5f6ab78336f31c88fc9413246f282737e
HugoSkola <151546996+HugoSkola@users.noreply.github.com>
2025-01-10 09:32:23 +0100

    Button to PE Class

--- Grupp 2.15/Assets/Scripts/Scene Loader.cs
+++ Grupp 2.15/Assets/Scripts/Scene Loader.cs
@@ -22,4 +22,8 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene("Art Class"); 
     }
+    public void LoadPEClass()
+    {
+        SceneManager.LoadScene("PE Class");
+    }
 }
