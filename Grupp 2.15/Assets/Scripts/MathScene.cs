d874207d8af771576ff71039bf7cad01d0243a78
DrDagErr <151174223+DrDagErr@users.noreply.github.com>
2025-01-10 10:08:15 +0100

    ?

---
+++ Grupp 2.15/Assets/Scripts/MathScene.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene01Events : MonoBehaviour
+{
+    public GameObject fadeScreenIn;
+    public GameObject charVelma;
+    public GameObject textBox;
+
+    void Start()
+    {
+        StartCoroutine(EventStarter());
+    }
+
+    IEnumerator EventStarter()
+    {
+        yield return new WaitForSeconds(2);
+        fadeScreenIn.SetActive(false);
+        charVelma.SetActive(true);
+
+        yield return new WaitForSeconds(2);
+        // this is where our text function will go in future tutorial
+        textBox.SetActive(true);
+
+        yield return new WaitForSeconds(2);
+       
+    }
+}
+
