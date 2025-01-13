eb00fc2bb18e8bb296071ee1f975fd4d65f71150
DrDagErr <151174223+DrDagErr@users.noreply.github.com>
2025-01-13 10:43:45 +0100

    texte

--- Grupp 2.15/Assets/Scripts/Scene startup.cs
+++ Grupp 2.15/Assets/Scripts/Scene startup.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class SceneStartup : MonoBehaviour
 {
+    
     public GameObject character;
     public GameObject textBox;
 
     [SerializeField] float waitSeconds;
+    [SerializeField] string textToSpeak;
+    [SerializeField] int currentTextLength;
+    [SerializeField] int textLength;
+    [SerializeField] GameObject mainTextObject;
+    [SerializeField] GameObject nextButton;
+    [SerializeField] int eventPos = 0; 
+
+    void Update()
+    {
+        textLength = TextCreater.charCount;
+    }
 
     void Start()
     {
@@ -16,16 +28,46 @@ public class SceneStartup : MonoBehaviour
 
     IEnumerator EventStarter()
     {
+        //event0
         yield return new WaitForSeconds(waitSeconds);
         //fadeScreenIn.SetActive(false);
         character.SetActive(true);
 
         yield return new WaitForSeconds(waitSeconds);
-        // this is where our text function will go in future tutorial
-        textBox.SetActive(true);
-
-        yield return new WaitForSeconds(waitSeconds);
-       
+        // this is where our text function will go
+        mainTextObject.SetActive(true);
+        textToSpeak = "bla bla bla lalall bal";
+        textBox.GetComponent <TMPro.TMP_Text>().text = textToSpeak;
+        currentTextLength = textToSpeak.Length;
+        TextCreater.runTextPrint = true; 
+        yield return new WaitForSeconds(0.05f);
+        yield return new WaitForSeconds(1);
+        yield return new WaitUntil(() => textLength == currentTextLength);
+        yield return new WaitForSeconds(0.5f);
+        nextButton.SetActive(true); 
+        eventPos = 1; 
+    }
+    IEnumerator EventOne()
+    {
+        //Event1
+        nextButton.SetActive(false);
+        textToSpeak = "Hello, my name is Velma";
+        textBox.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
+        currentTextLength = textToSpeak.Length;
+        TextCreater.runTextPrint = true;
+        yield return new WaitForSeconds(0.05f);
+        yield return new WaitForSeconds(1);
+        yield return new WaitUntil(() => textLength == currentTextLength);
+        yield return new WaitForSeconds(0.5f);
+        nextButton.SetActive(true);
+        eventPos = 2; 
+    }
+    public void NextButton()
+    {
+        if (eventPos == 1)
+        {
+            StartCoroutine(EventOne()); 
+        }
     }
 }
 
